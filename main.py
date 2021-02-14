8f1cdbdf1cf1681a4a3d457d85682bbc21af97dc
Garrett Neuenkirchen <garrettn@gmail.com>
2021-02-14 15:46:54 -0600

    first commit in the project

---
+++ main.py
@@ -0,0 +1,15 @@
+from Card import *
+from Deck import *
+from Game import *
+
+# card1 = Card(10, 2)
+# card2 = Card(11, 3)
+# print(card1 < card2)
+# print(card2)
+#
+# deck = Deck()
+# for card in deck.cards:
+#     print(card)
+
+mygame = Game()
+mygame.play_game()
